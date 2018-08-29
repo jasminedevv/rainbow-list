4a5aec7ef522da629e535c073bc5a84c5410039d
Jasmine Humbert <hello@jasminehumbert.com>
2018-08-28 20:23:10 -0700

    works but still using infinite loop

---
+++ rainbow-list.py
@@ -0,0 +1,77 @@
+checklist = list()
+
+print(checklist)
+
+def create(item):
+    checklist.append(item)
+
+def read(index):
+    return checklist[index]
+
+def update(index, item):
+    checklist[index] = item
+
+def destroy(index): checklist.pop(index)
+
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
+
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item: ")
+        create(input_item)
+
+    # Read item
+    elif function_code == "R":
+        item_index = int(user_input("Index Number? "))
+        # Remember that item_index must actually exist or our program will crash.
+        print(read(item_index))
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    # Catch all
+    else:
+        print("Unknown Option")
+
+def list_all_items():
+    index = 0
+    print("in my list I have:")
+    for list_item in checklist:
+        num = str(index)
+
+        print(list_item, " : ", num)
+        index += 1
+    print("and that's it!")
+
+# loop
+running = True
+while running:
+    selection = user_input(
+        "Press C to add to list, R to Read from list and P to display list\n> ")
+    select(selection)
+
+
+# tests
+def test():
+    select("C")
+    select("R")
+    select("P")
+    # list_all_items()
+    # print(read(0))
+    # print(read(1))
+    # update(0, "purple socks")
+    # destroy(1)
+    # print(read(0))
+    # print(read(1))
+
+create("blue")
+create("red")
+
+print(checklist)
+test()
